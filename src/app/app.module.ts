335f96ba37debd32886dcf735a25395cc2d49680
Rodolfo Thiede <rodolfo.thiede@forcast.cl>
2019-09-27 18:16:56 -0300

    latest changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BlogIndexComponent } from './components/blog-index/blog-index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogIndexComponent
   ],
   imports: [
     BrowserModule
